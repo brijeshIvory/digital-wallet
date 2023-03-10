eab0228904d94e9ffd5662d3b66d9abcf20a06ee
brijeshIvory <munbrijesh.ivorytechnolab@gmail.com>
2023-03-10 12:22:34 +0530

    navbar header

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,20 @@
 import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-
-import React from "react";
+import "./index.scss";
+// import Navlogo from "../../assets/img/Navlogo.jpg";
+import Divider from "@mui/material/Divider";
 
 function Navbar({ open, setOpen }) {
   return (
-    <div>
+    <div className="navbar">
       <Drawer open={open} onClose={() => setOpen(false)}>
-        hjgjjg
+        <div className="navbar_header">
+          {/* <img src={Navlogo} alt="navlogo" /> */}
+          <h1>Logo</h1>
+          <h4>+91 786785444</h4>
+          <Divider />
+          <h3>Wallet balance</h3>
+          <h3>0 </h3>
+        </div>
       </Drawer>
     </div>
   );
