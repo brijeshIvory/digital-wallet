8d51656f90a901e2afbf8c9cae3ca86f5ee54a46
brijeshIvory <munbrijesh.ivorytechnolab@gmail.com>
2023-03-10 18:03:28 +0530

    header and footer created

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,21 +1,13 @@
 import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
 import "./index.scss";
-import { Avatar } from "@mui/material";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import Navbar from "../Navbar";
 
 function Home() {
-  const [open, setOpen] = React.useState(false);
-
   return (
     <div className="home_container">
-      <div className="home_header">
-        <Avatar onClick={() => setOpen(true)} />
-        <Navbar open={open} setOpen={setOpen} />
-        <div className="home_headerRight">
-          <AccountBalanceIcon />
-        </div>
-      </div>
+      <Header />
+      <Footer />
     </div>
   );
 }
