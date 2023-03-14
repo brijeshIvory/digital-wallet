76b8d9d0f1807573d4804bc2b80e41e11a83f6a4
brijeshIvory <munbrijesh.ivorytechnolab@gmail.com>
2023-03-14 11:40:40 +0530

    wallet transfer page created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Router from "./Router/Router";
-import Auth from "./Components/Auth/index.jsx";
+// import Footer from "./Components/Footer";
+// import Header from "./Components/Header";
+// import Router from "./Router/Router";
+// import Auth from "./Components/Auth/index.jsx";
+import WalletTransfer from "./Components/WalletTransfer";
+import Profile from "./Components/Profile/index.jsx";
 function App() {
-  const isLoggedIn = true;
+  // const isLoggedIn = true;
   return (
     <div className="app">
-      <Header />
+      {/* <Header /> */}
       {/* <Router /> */}
-      <Auth />
-      <Footer />
+      {/* <Auth /> */}
+      {/* <Footer /> */}
+      <WalletTransfer />
+      {/* <Profile /> */}
     </div>
   );
 }
