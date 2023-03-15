cf0f77c02a23267c4691cb8f5eb0b4c8ad3dcacf
brijeshIvory <munbrijesh.ivorytechnolab@gmail.com>
2023-03-15 18:20:30 +0530

    transactions mock data added

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <div className="app">
       {/* <Passbook /> */}
-      {/* <NewHomePage /> */}
+      <NewHomePage />
       {/* <Router />
       <Footer /> */}
       {/* <ReferralPopup /> */}
-      <HomeDashboard />
+      {/* <HomeDashboard /> */}
     </div>
   );
 }
--- src/Components/NewHomePage/index.jsx
+++ src/Components/NewHomePage/index.jsx
@@ -6,16 +6,40 @@ import { AiOutlineTransaction } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
 
 function NewHomePage() {
+  const data = [
+    {
+      to: "Party1",
+      amount: "+78",
+      Date: "02/03/2023",
+    },
+    {
+      to: "Party2",
+      amount: "+89",
+      Date: "15/02/2023",
+    },
+    {
+      to: "Party3",
+      amount: "+97",
+      Date: "25/02/2023",
+    },
+    {
+      to: "Party4",
+      amount: "+63",
+      Date: "28/01/2023",
+    },
+  ];
   return (
     <div className="new-home-page">
       <div className="new-home-page-topdiv">
         <div className="new-home-page-header">
-          <div className="new-home-page-header-avatar">
-            <RxAvatar />
-          </div>
           <div>
             <div>Hello,</div>
-            <h3>USER USER</h3>
+            <div style={{ marginTop: "0.5rem", fontWeight: 700 }}>
+              USER USER
+            </div>
+          </div>
+          <div className="new-home-page-header-avatar">
+            <RxAvatar />
           </div>
         </div>
         <div className="new-home-page-balance">
@@ -46,8 +70,19 @@ function NewHomePage() {
 
       <div className="latest-transaction">
         <div className="latest-transaction-title">
-          <div>Latest Transaction</div>
-          <div>See All</div>
+          <div>Latest Transactions</div>
+          <div style={{ color: "#595858" }}>See All</div>
+        </div>
+        <div className="latest-transaction-data">
+          {data.map((ele) => (
+            <div className="latest-transaction-single-data">
+              <div>
+                <div className="latest-transaction-date">{ele.Date}</div>
+                <div className="latest-transaction-party">{ele.to}</div>
+              </div>
+              <div>{ele.amount}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
