0f0bee310944b82246161bf5705e40d2e66c6b67
nirali <nirali.ivorytechnolab@gmail.com>
2023-03-22 16:58:32 +0530

    env file setup

--- src/App/api/transactionApi.js
+++ src/App/api/transactionApi.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-const base_url = "https://paindustry.in/booky/api";
+export const base_url = process.env.REACT_APP_API_URL
 
 export function* GetTransactionsApi(transactionPayload) {
-  const transactions = yield axios
-    .post(`${base_url}/transactiondetails`, transactionPayload)
-    .then(function (response) {
-      return response;
-    })
-    .catch(function (error) {
-      return error.response;
-    });
-  return transactions;
-}
+    const transactions = yield axios
+      .post(`${base_url}/transactiondetails`, transactionPayload)
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error.response;
+      });
+    return transactions;
+  }
+  
