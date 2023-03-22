07d0c29504425489cf4444e15501de58a2b38c2d
nirali <nirali.ivorytechnolab@gmail.com>
2023-03-22 10:09:45 +0530

    password validation

--- src/utills/ValidationSchema.js
+++ src/utills/ValidationSchema.js
@@ -46,11 +46,13 @@ export const RegistationValidationSchema = yup.object({
   country_code: yup.string().required('please select Country code'),
 
   phone: yup.string().required('Phone No is required !'),
-  password: yup.string().required('please Enter password !'),
-
+  password: yup.string()
+  .min(6, 'Password must be at least 6 characters')
+  .max(24, 'Password can be maximum 24 characters')
+  .required('Required'),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match'),
+    .oneOf([yup.ref('password'), null], 'Passwords not match'),
 })
 export const DepositeAmountValidationSchema = yup.object({
   amount: yup
@@ -79,7 +81,7 @@ export const ForgotPasswordValidationSchema = yup.object({
     )
     .required('Email is required !'),
 
-  password: yup.string().required('password is required !'),
+  password: yup.string().min(8, "Password must be 8 character").required("Password is Required"),
 
   confirm_password: yup.string().required('please add confrim password'),
 })
